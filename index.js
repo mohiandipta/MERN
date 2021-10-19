58f4b215b5cbbfc6dc4177c81dee884d447fda10
mohiandipta <mohianmostofa@gmail.com>
2021-10-19 23:17:58 +0600

    middleWare installed and added to the index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express();
 
+// using middleWare for CORS
+const myMiddleWare = (req, res, next) => {
+    console.log('MiddleWare  Applied!')
+    next();
+}
+app.use(myMiddleWare);
+
+let user = {
+    name: 'Mohian',
+    Age: 23,
+    Company: 'Akij',
+    Salary: '20,000'
+}
 
 app.get(`/api`, (req, res) => {
-    res.send("Hello")
+    res.json(user)
 })
 
 app.listen(8080, () => {
--- view/src/App.js
+++ view/src/App.js
@@ -1,46 +1,74 @@
 import { useState } from "react";
 
-function App() {
+// fetch
+const App = () => {
+  const [user, setUser] = useState('');
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("");
+  const fetchUserData = () => {
+    // let endPointURL = 'http://localhost:8080/api'
 
-  // function for submit and get data from events
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('STATE => ', email, password)
+    fetch(`http://localhost:8080/api`, {
+      method: 'GET',
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => console.log(err))
   }
-
   return (
-    <div className="Container">
-      <h1 className='display-4 text-center'>Learning React</h1>
-
-      <div className='row'>
-        <div className='col-md-6 offset-md-3'>
-          <form onSubmit={handleSubmit}>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input
-                value={email} //dataBind with useState
-                onChange={event => setEmail(event.target.value)} // onChange event
-                type="email"
-                class="form-control"
-              />
-              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input
-                value={password}
-                onChange={event => setPassword(event.target.value)}
-                type="password" class="form-control" />
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
-        </div>
-      </div>
+    <div className='Container'>
+      <button onClick={fetchUserData} className='btn btn-primary'>
+        Fetch User Data
+      </button>
     </div>
-  );
+  )
 }
 
 export default App;
+
+// import { useState } from "react";
+
+// function App() {
+
+//   const [email, setEmail] = useState("")
+//   const [password, setPassword] = useState("");
+
+//   // function for submit and get data from events
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     console.log('STATE => ', email, password)
+//   }
+
+//   return (
+//     <div className="Container">
+//       <h1 className='display-4 text-center'>Learning React</h1>
+
+//       <div className='row'>
+//         <div className='col-md-6 offset-md-3'>
+//           <form onSubmit={handleSubmit}>
+//             <div class="mb-3">
+//               <label for="exampleInputEmail1" class="form-label">Email address</label>
+//               <input
+//                 value={email} //dataBind with useState
+//                 onChange={event => setEmail(event.target.value)} // onChange event
+//                 type="email"
+//                 class="form-control"
+//               />
+//               <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+//             </div>
+//             <div class="mb-3">
+//               <label for="exampleInputPassword1" class="form-label">Password</label>
+//               <input
+//                 value={password}
+//                 onChange={event => setPassword(event.target.value)}
+//                 type="password" class="form-control" />
+//             </div>
+//             <button type="submit" class="btn btn-primary">Submit</button>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
