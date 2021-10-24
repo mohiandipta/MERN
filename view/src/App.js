7ee8082647d354aa2394cac5ed02a24aebdd9b4e
mohiandipta <mohianmostofa@gmail.com>
2021-10-24 22:46:33 +0600

    using axios for fetching data

--- view/src/App.js
+++ view/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 // fetch
 const App = () => {
@@ -11,16 +12,23 @@ const App = () => {
 
   //function for userData fetching
   const fetchUserData = () => {
-    // let endPointURL = 'http://localhost:8080/api'
-    fetch(`http://localhost:8080/api/users`, {
-      method: 'GET',
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => setUsers(data.users))
-      .catch((err) => console.log(err))
+    // using axios for fetching data
+    axios
+      .get(`http://localhost:8080/api/users`)
+      .then(data => setUsers(data.users))
+      .catch(err => console.log(err));
   }
+  //////////
+  // let endPointURL = 'http://localhost:8080/api'
+  //   fetch(`http://localhost:8080/api/users`, {
+  //     method: 'GET',
+  //   })
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => setUsers(data.users))
+  //     .catch((err) => console.log(err))
+  // }
 
   //
   const fetchPost = () => {
@@ -58,49 +66,49 @@ const App = () => {
 
 export default App;
 
-// import { useState } from "react";
+  // import { useState } from "react";
 
-// function App() {
+  // function App() {
 
-//   const [email, setEmail] = useState("")
-//   const [password, setPassword] = useState("");
+  //   const [email, setEmail] = useState("")
+  //   const [password, setPassword] = useState("");
 
-//   // function for submit and get data from events
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     console.log('STATE => ', email, password)
-//   }
+  //   // function for submit and get data from events
+  //   const handleSubmit = (event) => {
+  //     event.preventDefault();
+  //     console.log('STATE => ', email, password)
+  //   }
 
-//   return (
-//     <div className="Container">
-//       <h1 className='display-4 text-center'>Learning React</h1>
+  //   return (
+  //     <div className="Container">
+  //       <h1 className='display-4 text-center'>Learning React</h1>
 
-//       <div className='row'>
-//         <div className='col-md-6 offset-md-3'>
-//           <form onSubmit={handleSubmit}>
-//             <div class="mb-3">
-//               <label for="exampleInputEmail1" class="form-label">Email address</label>
-//               <input
-//                 value={email} //dataBind with useState
-//                 onChange={event => setEmail(event.target.value)} // onChange event
-//                 type="email"
-//                 class="form-control"
-//               />
-//               <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-//             </div>
-//             <div class="mb-3">
-//               <label for="exampleInputPassword1" class="form-label">Password</label>
-//               <input
-//                 value={password}
-//                 onChange={event => setPassword(event.target.value)}
-//                 type="password" class="form-control" />
-//             </div>
-//             <button type="submit" class="btn btn-primary">Submit</button>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//   );
+  //       <div className='row'>
+  //         <div className='col-md-6 offset-md-3'>
+  //           <form onSubmit={handleSubmit}>
+  //             <div class="mb-3">
+  //               <label for="exampleInputEmail1" class="form-label">Email address</label>
+  //               <input
+  //                 value={email} //dataBind with useState
+  //                 onChange={event => setEmail(event.target.value)} // onChange event
+  //                 type="email"
+  //                 class="form-control"
+  //               />
+  //               <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  //             </div>
+  //             <div class="mb-3">
+  //               <label for="exampleInputPassword1" class="form-label">Password</label>
+  //               <input
+  //                 value={password}
+  //                 onChange={event => setPassword(event.target.value)}
+  //                 type="password" class="form-control" />
+  //             </div>
+  //             <button type="submit" class="btn btn-primary">Submit</button>
+  //           </form>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
 // }
 
 // export default App;
