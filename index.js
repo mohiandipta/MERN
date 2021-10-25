1beecf158661499016ea68ff8f517a13c82239e0
mohiandipta <mohianmostofa@gmail.com>
2021-10-25 17:38:23 +0600

    mongoose installed, db connection created

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const express = require('express')
 const app = express();
 const cors = require('cors')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+// db connection
+mongoose.connect("mongodb+srv://mohiandipta:12345@cluster0.mbto5.mongodb.net/mern?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        dbName: 'mern'
+    })
+    .then(() => {
+        console.log('database connection is ready')
+    })
+    .catch((err) => {
+        console.log('database can not be connected')
+    })
+
 
 // using middleWare
 app.use(cors());
